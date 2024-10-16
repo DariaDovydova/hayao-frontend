2716a14bbd401cb01ec6d3bceee2474a0be5f798
DariaDovydova <vip.dovydova@list.ru>
2024-10-16 17:00:46 +0300

    test

--- src/Stripe/CheckoutForm.jsx
+++ src/Stripe/CheckoutForm.jsx
@@ -19,7 +19,7 @@ export const CheckoutForm = () => {
       try {
         const { id } = paymentMethod;
         const response = await axios.post(
-          "http://localhost:8080/stripe/charge",
+          "https://hayao-backend-1.onrender.com//stripe/charge",
           {
             amount: 999,
             id: id,
